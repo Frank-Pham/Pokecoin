f4a0b7171449d44a555d2f38e48578feb546ef82
ilias_na <ilias.nasri@student.hs-rm.de>
2021-01-15 19:04:06 +0100

    Pack Opening dialog

--- src/pages/shop/CardShop.js
+++ src/pages/shop/CardShop.js
@@ -1,19 +1,28 @@
 import { Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import react, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import RESTConstans from "../../utiels/constans/RESTConstans";
 import { UserContext } from "../../context/user/UserContext";
 import axios from "axios";
 import CardPackage from "../../models/CardPackage";
 import CardShopHeader from "./CardShopHeader";
-import React from "react";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import PokemonPack from "../../assets/images/PokemonPack.jpg";
+import PokemonList from "../../models/PokemonList";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import {ResizableBox} from "react-resizable";
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
 
+const transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 const useStyles = makeStyles((theme) => ({
   grid: {
     width: "100%",
@@ -41,6 +50,8 @@ export default function CardShop() {
   const classes = useStyles();
   const { token } = useContext(UserContext);
   const { user,setUser } = useContext(UserContext);
+  const [pokemon, setPokemon] = useState([]);
+  const [openPack, setOpenPack] = useState(false);
   const [packs, setPacks] = useState([]);
   const [userName, setUserName] = useState("");
   const [coins, setCoins] = useState(0);
@@ -61,7 +72,7 @@ export default function CardShop() {
       RESTConstans.DOMAIN + RESTConstans.PACKAGES
     );
     setPacks((packs) => ["base"]);
-    console.log("Die Pack-Auswahl" + packArray);
+    console.log("Die Pack-Auswahl " + packArray);
     for (const pack of packs) {
       console.log(pack);
     }
@@ -78,13 +89,21 @@ export default function CardShop() {
   }
 
   const buyPack = async() => {
+    setOpenPack(true);
     await axios.get(RESTConstans.DOMAIN + RESTConstans.PACKAGES + "/Base" + RESTConstans.DEFAULT_PACK,{
       headers:{
         token: token
       }
-    }).then((response) => console.log(response.data)).catch((error) => console.log(error))
-  }
+    }).then((response) =>{
+      const cards = response.data.cards;
+      setPokemon([...cards])
+      console.log(cards.cards[0].id);
+    }).catch((error) => console.log(error))
 
+  }
+  const closePack= () => {
+    setOpenPack(false);
+  }
   /**
    * fetchen ausgelagert
    *
@@ -125,6 +144,34 @@ export default function CardShop() {
         </ButtonGroup>
       </div>
       <Button variant="contained" color="primary" onClick={buyPack}>Kaufen</Button>
+      <ResizableBox
+      height={400}
+      width={600}
+      >
+      <Dialog
+          open={openPack}
+          TransitionComponent={transition}
+          keepMounted
+          onClose={closePack}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Pack Cards"}</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={3} className={classes.grid}>
+            <Grid item xs={12}>
+              <PokemonList pokemon={pokemon}></PokemonList>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closePack} color="primary">
+            Close and accept Pack
+          </Button>
+        </DialogActions>
+      </Dialog>
+      </ResizableBox>
     </Grid>
+
   );
 }
