a5e0ff92e1309b9e73fa1af55550bdd0689512f2
Raoul  Sudiana <raouls@Raouls-Air.fritz.box>
2021-01-26 14:11:19 +0100

    theme added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import CardDetails from "./pages/cards/CardDetails";
 import { CardContext } from "./context/user/CardContext";
 import ChangePassword from "./pages/changePassword/ChangePassword";
 import Profile from "./pages/profile/Profile";
+import Colors from "./themes/Colors";
 
 function App() {
   const [userCreds, setUserCreds] = useState({
@@ -25,43 +26,54 @@ function App() {
   const isLoggedIn = () => (userCreds.token != null ? <Navbar /> : "");
 
   return (
-    <Router>
-      <UserContext.Provider value={{ userCreds, setUserCreds }}>
-        {isLoggedIn()}
+    <div
+      style={{
+        backgroundColor: Colors.background,
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: "absolute",
+      }}
+    >
+      <Router>
+        <UserContext.Provider value={{ userCreds, setUserCreds }}>
+          {isLoggedIn()}
 
-        <Switch>
-          <Route exact path="/">
-            <LoginPage />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/register">
-            <RegisterPage />
-          </Route>
-          <Route path="/changePassword">
-            <ChangePassword />
-          </Route>
-          <Route path="/main">
-            <MainPage />
-          </Route>
-          <Route path="/cardShop">
-            <CardShop></CardShop>
-          </Route>
-          <CardContext.Provider value={{ cardID, setCardID }}>
-            <Route path="/cardCollection">
-              <CardCollection></CardCollection>
+          <Switch>
+            <Route exact path="/">
+              <LoginPage />
             </Route>
-            <Route path="/cards">
-              <Cards></Cards>
+            <Route path="/profile">
+              <Profile />
             </Route>
-            <Route path="/cardDetail/:cardId">
-              <CardDetails>halloooo</CardDetails>
+            <Route path="/register">
+              <RegisterPage />
             </Route>
-          </CardContext.Provider>
-        </Switch>
-      </UserContext.Provider>
-    </Router>
+            <Route path="/changePassword">
+              <ChangePassword />
+            </Route>
+            <Route path="/main">
+              <MainPage />
+            </Route>
+            <Route path="/cardShop">
+              <CardShop></CardShop>
+            </Route>
+            <CardContext.Provider value={{ cardID, setCardID }}>
+              <Route path="/cardCollection">
+                <CardCollection></CardCollection>
+              </Route>
+              <Route path="/cards">
+                <Cards></Cards>
+              </Route>
+              <Route path="/cardDetail/:cardId">
+                <CardDetails>halloooo</CardDetails>
+              </Route>
+            </CardContext.Provider>
+          </Switch>
+        </UserContext.Provider>
+      </Router>
+    </div>
   );
 }
 
--- src/themes/Colors.js
+++ src/themes/Colors.js
@@ -1,7 +1,7 @@
 const Colors = {
   primary: "#3E58B5",
   secondary: "#4C4298",
-  background: "#yellow",
+  background: "#8295D7",
   // accent: "#ACCAF2",
 };
 
